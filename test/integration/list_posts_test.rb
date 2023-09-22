51dc6e76871a62b9aa9671423ee64b88f6447470
Haroon Ahmed <haroon.ahmed25@gmail.com>
2023-09-22 23:10:06 +0100

    clean up'

--- test/integration/list_posts_test.rb
+++ test/integration/list_posts_test.rb
@@ -3,10 +3,6 @@ require "test_helper"
   require "application_system_test_case"
 
   class ListPosts < ApplicationSystemTestCase
-    if ENV["CI"]
-      driven_by :selenium, using: :headless_chrome
-    end
-
     test "See post" do
       visit posts_path
       assert_text 'Posts'
