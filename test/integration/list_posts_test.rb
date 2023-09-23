3e7e36deeada5b407c30a0e9a97a9677b7213faa
Haroon Ahmed <haroon.ahmed25@gmail.com>
2023-09-23 22:29:03 +0100

    override in base test class instead

--- test/integration/list_posts_test.rb
+++ test/integration/list_posts_test.rb
@@ -3,8 +3,6 @@ require "test_helper"
   require "application_system_test_case"
 
   class ListPosts < ApplicationSystemTestCase
-    driven_by :selenium, using: :headless_chrome
-
     test "See post" do
       visit posts_path
       assert_text 'Posts'
